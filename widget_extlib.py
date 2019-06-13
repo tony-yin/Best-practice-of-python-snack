57aef7ae21ec6097fbf791c28f959e0a52696890
yinhui <hui.yin@uniswdc.com>
2019-06-14 01:39:59 +0800

    :sparkles: 1. format code with pep8; 2. add ExtEntryRadioWindow widget.

--- widget_extlib.py
+++ widget_extlib.py
@@ -1,17 +1,19 @@
 import time
-from snack import *
+from snack import ButtonBar, TextboxReflowed, GridFormHelp, GridForm, \
+    Textbox, CheckboxTree, Listbox, Grid, Entry, Label, types, Scale, \
+    SingleRadioButton
 
 
 def ExtButtonChoiceWindow(screen, title, text,
-                buttons = [ 'Ok', 'Cancel' ],
-                width = 40, x = None, y = None, help = None):
+                          buttons=['Ok', 'Cancel'],
+                          width=40, x=None, y=None, help=None):
 
     bb = ButtonBar(screen, buttons, compact=1)
-    t = TextboxReflowed(width, text, maxHeight = screen.height - 12)
+    t = TextboxReflowed(width, text, maxHeight=screen.height - 12)
 
     g = GridFormHelp(screen, title, help, 1, 2)
-    g.add(t, 0, 0, padding = (0, 0, 0, 1))
-    g.add(bb, 0, 1, growx = 1)
+    g.add(t, 0, 0, padding=(0, 0, 0, 1))
+    g.add(bb, 0, 1, growx=1)
     return bb.buttonPressed(g.runOnce(x, y))
 
 
@@ -20,7 +22,7 @@ def ExtAlert(screen, title, msg, width=70):
 
 
 def ExtCheckboxWindow(screen, title, text, items,
-                buttons = ('Ok', 'Cancel'), width = 50, height=8):
+                      buttons=('Ok', 'Cancel'), width=50, height=8):
 
     g = GridForm(screen, title, 1, 3)
     g.add(Textbox(width, 2, text), 0, 0)
@@ -43,18 +45,19 @@ def ExtCheckboxWindow(screen, title, text, items,
 
 
 def ExtListboxChoiceWindow(screen, title, text, items,
-                buttons = ('Ok', 'Cancel'),
-                width = 40, scroll = 0, height = -1,
-                default = None, help = None):
+                           buttons=('Ok', 'Cancel'),
+                           width=40, scroll=0, height=-1,
+                           default=None, help=None):
 
-    if (height == -1): height = len(items)
+    if (height == -1):
+        height = len(items)
 
     bb = ButtonBar(screen, buttons, compact=1)
     t = TextboxReflowed(width, text)
-    l = Listbox(height, scroll = scroll, returnExit = 1)
+    lb = Listbox(height, scroll=scroll, returnExit=1)
     count = 0
     for item in items:
-        if (type(item) == types.TupleType):
+        if isinstance(item, types.TupleType):
             (text, key) = item
         else:
             text = item
@@ -65,27 +68,27 @@ def ExtListboxChoiceWindow(screen, title, text, items,
         elif (default == item):
             default = key
 
-        l.append(text, key)
+        lb.append(text, key)
         count = count + 1
 
-    if (default != None):
-        l.setCurrent (default)
+    if (default is not None):
+        lb.setCurrent(default)
 
     g = GridFormHelp(screen, title, help, 1, 3)
     g.add(t, 0, 0)
-    g.add(l, 0, 1, padding = (0, 1, 0, 1))
-    g.add(bb, 0, 2, growx = 1)
+    g.add(lb, 0, 1, padding=(0, 1, 0, 1))
+    g.add(bb, 0, 2, growx=1)
 
     rc = g.runOnce()
 
-    return (rc, bb.buttonPressed(rc), l.current())
+    return (rc, bb.buttonPressed(rc), lb.current())
 
 
 def ExtEntryWindow(screen, title, text, prompts,
-        allowCancel = 1, width = 40, entryWidth = 20,
-        buttons = [ 'Ok', 'Cancel' ], help = None):
+                   allowCancel=1, width=40, entryWidth=20,
+                   buttons=['Ok', 'Cancel'], help=None):
 
-    bb = ButtonBar(screen, buttons, compact=1);
+    bb = ButtonBar(screen, buttons, compact=1)
     t = TextboxReflowed(width, text)
 
     count = 0
@@ -97,23 +100,23 @@ def ExtEntryWindow(screen, title, text, prompts,
     count = 0
     entryList = []
     for n in prompts:
-        if (type(n) == types.TupleType):
+        if isinstance(n, types.TupleType):
             (n, e) = n
             if (type(e) in types.StringTypes):
                 e = Entry(entryWidth, e)
         else:
             e = Entry(entryWidth)
 
-        sg.setField(Label(n), 0, count, padding = (0, 0, 1, 0), anchorLeft = 1)
-        sg.setField(e, 1, count, anchorLeft = 1)
+        sg.setField(Label(n), 0, count, padding=(0, 0, 1, 0), anchorLeft=1)
+        sg.setField(e, 1, count, anchorLeft=1)
         count = count + 1
         entryList.append(e)
 
     g = GridFormHelp(screen, title, help, 1, 3)
 
-    g.add(t, 0, 0, padding = (0, 0, 0, 1))
-    g.add(sg, 0, 1, padding = (0, 0, 0, 1))
-    g.add(bb, 0, 2, growx = 1)
+    g.add(t, 0, 0, padding=(0, 0, 0, 1))
+    g.add(sg, 0, 1, padding=(0, 0, 0, 1))
+    g.add(bb, 0, 2, growx=1)
 
     result = g.runOnce()
 
@@ -127,10 +130,10 @@ def ExtEntryWindow(screen, title, text, prompts,
 
 
 def ExtPwdEntryWindow(screen, title, text, prompts,
-        allowCancel = 1, width = 40, entryWidth = 20,
-        buttons = [ 'Ok', 'Cancel' ], help = None):
+                      allowCancel=1, width=40, entryWidth=20,
+                      buttons=['Ok', 'Cancel'], help=None):
 
-    bb = ButtonBar(screen, buttons, compact=1);
+    bb = ButtonBar(screen, buttons, compact=1)
     t = TextboxReflowed(width, text)
 
     count = 0
@@ -142,12 +145,12 @@ def ExtPwdEntryWindow(screen, title, text, prompts,
     count = 0
     entryList = []
     for n in prompts:
-        if (type(n) == types.TupleType):
+        if isinstance(n, types.TupleType):
             (n, e) = n
             if (type(e) in types.StringTypes):
-                e = Entry(entryWidth, e, password = 1)
+                e = Entry(entryWidth, e, password=1)
         else:
-            e = Entry(entryWidth, password = 1)
+            e = Entry(entryWidth, password=1)
 
         sg.setField(Label(n), 0, count, padding=(0, 0, 1, 0), anchorRight=1)
         sg.setField(e, 1, count, anchorLeft=1)
@@ -156,9 +159,9 @@ def ExtPwdEntryWindow(screen, title, text, prompts,
 
     g = GridFormHelp(screen, title, help, 1, 3)
 
-    g.add(t, 0, 0, padding = (0, 0, 0, 1))
-    g.add(sg, 0, 1, padding = (0, 0, 0, 1))
-    g.add(bb, 0, 2, growx = 1)
+    g.add(t, 0, 0, padding=(0, 0, 0, 1))
+    g.add(sg, 0, 1, padding=(0, 0, 0, 1))
+    g.add(bb, 0, 2, growx=1)
 
     result = g.runOnce()
 
@@ -171,13 +174,102 @@ def ExtPwdEntryWindow(screen, title, text, prompts,
     return (result, bb.buttonPressed(result), tuple(entryValues))
 
 
+def ExtEntryRadioWindow(screen, title, text, prompts,
+                        allowCancel=1, width=40, entryWidth=20,
+                        buttons=['Ok', 'Cancel'], help=None, radio_prompts=''):
+
+    bb = ButtonBar(screen, buttons, compact=1)
+    t = TextboxReflowed(width, text)
+    radio_grid = Grid(3, len(radio_prompts))
+    sg = Grid(2, len(prompts))
+
+    max_name_length = 0
+    for n in prompts:
+        if isinstance(n, types.TupleType):
+            n = n[0]
+        if len(n) > max_name_length:
+            max_name_length = len(n)
+
+    radioList = parse_radio_prompts(radio_prompts, radio_grid, entryWidth,
+                                    max_name_length)
+    entryList = []
+    entry_row = 0
+    for n in prompts:
+        if isinstance(n, types.TupleType):
+            (n, e) = n
+            if (type(e) in types.StringTypes):
+                e = Entry(entryWidth, e)
+        else:
+            e = Entry(entryWidth)
+
+        sg.setField(Label(n), 0, entry_row, padding=(0, 0, 1, 0), anchorLeft=1)
+        sg.setField(e, 1, entry_row, anchorLeft=1)
+        entry_row += 1
+        entryList.append(e)
+
+    g = GridFormHelp(screen, title, help, 1, 4)
+    g.add(t, 0, 0, padding=(0, 0, 0, 1))
+    g.add(radio_grid, 0, 1, padding=(0, 0, 0, 1))
+    g.add(sg, 0, 2, padding=(0, 0, 0, 1))
+    g.add(bb, 0, 3, growx=1)
+
+    result = g.runOnce()
+
+    entryValues = []
+    for rowRadioList in radioList:
+        for radio, radio_text in rowRadioList:
+            if radio.selected():
+                entryValues.append(radio_text)
+                break
+    for entry in entryList:
+        entryValues.append(entry.value())
+
+    return (result, bb.buttonPressed(result), tuple(entryValues))
+
+
+def parse_radio_prompts(radio_prompts, radio_grid, entryWidth,
+                        max_name_length):
+    radioList = []
+    radio_row = 0
+    for n in radio_prompts:
+        label_text, radio_text_list, on_radio_text = n
+        group = None
+        radio_column = 1
+        row_radio_list_length = len(radio_text_list)
+        rowRadioList = []
+        for radio_text in radio_text_list:
+            is_on = 1 if radio_text == on_radio_text else 0
+            r = SingleRadioButton(radio_text, group, is_on)
+            group = r
+            radio_text_length = 4 + len(radio_text)
+            radio_right_padding = (entryWidth / row_radio_list_length) - \
+                radio_text_length
+            remainder = entryWidth % row_radio_list_length
+            if remainder != 0 and row_radio_list_length == radio_column:
+                radio_right_padding = radio_right_padding + remainder
+            radio_grid.setField(r, radio_column, radio_row,
+                                padding=(0, 0, radio_right_padding, 0),
+                                anchorLeft=1)
+            radio_column += 1
+            rowRadioList.append((r, radio_text))
+
+        label_text_length = len(label_text)
+        label_right_padding = max_name_length - label_text_length + 1
+        radio_grid.setField(Label(label_text), 0, radio_row,
+                            padding=(0, 0, label_right_padding, 0),
+                            anchorLeft=1)
+        radio_row += 1
+        radioList.append(rowRadioList)
+    return radioList
+
+
 class ExtProgressWindow:
 
     def __init__(self, screen, title, text):
         self.screen = screen
         self.g = GridForm(self.screen, title, 1, 2)
         self.s = Scale(70, 100)
-        self.t = Textbox(70, 8, text)
+        self.t = Textbox(70, 12, text)
         self.g.add(self.t, 0, 0)
         self.g.add(self.s, 0, 1)
 
@@ -185,14 +277,38 @@ class ExtProgressWindow:
         self.g.draw()
         self.screen.refresh()
 
-    def update(self, progress, text = ''):
+    def update(self, progress, text=''):
         self.s.set(progress)
         self.t.setText(text)
         self.g.draw()
         self.screen.refresh()
 
     def close(self):
-        #self.update(100)
+        time.sleep(1)
+        self.screen.popWindow()
+
+
+class ExtTopProgressWindow:
+
+    def __init__(self, screen, title, text):
+        self.screen = screen
+        self.g = GridForm(self.screen, title, 1, 2)
+        self.s = Scale(70, 100)
+        self.t = Textbox(70, 12, text)
+        self.g.add(self.s, 0, 0)
+        self.g.add(self.t, 0, 1, padding=(0, 1, 0, 0))
+
+    def show(self):
+        self.g.draw()
+        self.screen.refresh()
+
+    def update(self, progress, text=''):
+        self.s.set(progress)
+        self.t.setText(text)
+        self.g.draw()
+        self.screen.refresh()
+
+    def close(self):
         time.sleep(1)
         self.screen.popWindow()
 
